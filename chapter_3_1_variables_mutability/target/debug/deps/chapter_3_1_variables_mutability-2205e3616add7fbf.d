a2df79c56374df779a8492f17bdaa9d40a3c065a
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-07-01 22:18:12 +0530

    not much changes

---
+++ chapter_3_1_variables_mutability/target/debug/deps/chapter_3_1_variables_mutability-2205e3616add7fbf.d
@@ -0,0 +1,9 @@
+/home/mdkarch/1_Projects/2_rust-Programming/chapter_3_1_variables_mutability/target/debug/deps/libchapter_3_1_variables_mutability-2205e3616add7fbf.rmeta: src/main.rs Cargo.toml
+
+/home/mdkarch/1_Projects/2_rust-Programming/chapter_3_1_variables_mutability/target/debug/deps/chapter_3_1_variables_mutability-2205e3616add7fbf.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
