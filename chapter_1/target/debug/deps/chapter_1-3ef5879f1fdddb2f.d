3617a374a41982f3cd67624935c93b50375b7625
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-07-14 18:40:56 +0530

    moved all the target files to the Global Directory Away From Home.

--- chapter_1/target/debug/deps/chapter_1-3ef5879f1fdddb2f.d
+++
@@ -1,5 +0,0 @@
-/home/mukuldk/1Home/1Projects/class/rust-Programming/chapter_1/target/debug/deps/chapter_1-3ef5879f1fdddb2f: src/main.rs
-
-/home/mukuldk/1Home/1Projects/class/rust-Programming/chapter_1/target/debug/deps/chapter_1-3ef5879f1fdddb2f.d: src/main.rs
-
-src/main.rs:
--- chapter_1/target/debug/deps/chapter_1-5931d56ba5e5ad5a.d
+++
@@ -1,5 +0,0 @@
-/home/mdkarch/1_Projects/2_rust-Programming/chapter_1/target/debug/deps/libchapter_1-5931d56ba5e5ad5a.rmeta: src/main.rs
-
-/home/mdkarch/1_Projects/2_rust-Programming/chapter_1/target/debug/deps/chapter_1-5931d56ba5e5ad5a.d: src/main.rs
-
-src/main.rs:
