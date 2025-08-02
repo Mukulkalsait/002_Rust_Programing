0e51ec558f5b0c51ee5c11d4acc2ff6fde1ccba8
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-02 22:39:29 +0530

    Learning Scructs

--- chapter_7_scruct/src/main.rs
+++ chapter_7_scruct/src/main.rs
@@ -1,94 +1,83 @@
-fn main() {
-    struct_learning();
-    // advanced_standard_fmt_1();
+#![allow(dead_code)] // at the top.
+
+#[derive(Debug)] // Without this {:?} will not work in struct.
+struct Person {
+    name: String,
+    age: u8,
 }
 
+struct Unit; // A unit struct
+struct Pair(i32, f32); // A tuple struct
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Y:  Structs in Structs : Coordinates of Rectangle using 'Point 👆🏽'
+struct Rectangle {
+    top_left: Point,
+    top_right: Point,
+    bottom_left: Point,
+    bottom_right: Point,
+}
+
+// R: =================================================================
+
 fn struct_learning() {
-    // An attribute to hide warnings for unused code.
-    #![allow(dead_code)]
+    // Create struct with field init shorthand
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
 
-    #[derive(Debug)]
-    struct Person {
-        name: String,
-        age: u8,
-    }
+    // Print debug struct
+    println!("{:?}", peter);
 
-    // A unit struct
-    struct Unit;
+    // Instantiate a `Point`
+    let point: Point = Point { x: 5.2, y: 0.4 };
+    let another_point: Point = Point { x: 10.3, y: 0.2 };
 
-    // A tuple struct
-    struct Pair(i32, f32);
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
 
-    // A struct with two fields
-    struct Point {
-        x: f32,
-        y: f32,
-    }
+    // Make a new point by using struct update syntax to use the fields of our
+    // other one
+    let bottom_right = Point {
+        x: 10.3,
+        ..another_point
+    };
 
-    // Structs can be reused as fields of another struct
-    struct Rectangle {
-        // A rectangle can be specified by where the top left and bottom right
-        // corners are in space.
-        top_left: Point,
-        bottom_right: Point,
-    }
+    // `bottom_right.y` will be the same as `another_point.y` because we used that field
+    // from `another_point`
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
 
-    fn main() {
-        // Create struct with field init shorthand
-        let name = String::from("Peter");
-        let age = 27;
-        let peter = Person { name, age };
+    // Destructure the point using a `let` binding
+    let Point {
+        x: left_edge,
+        y: top_edge,
+    } = point;
 
-        // Print debug struct
-        println!("{:?}", peter);
-
-        // Instantiate a `Point`
-        let point: Point = Point { x: 5.2, y: 0.4 };
-        let another_point: Point = Point { x: 10.3, y: 0.2 };
-
-        // Access the fields of the point
-        println!("point coordinates: ({}, {})", point.x, point.y);
-
-        // Make a new point by using struct update syntax to use the fields of our
-        // other one
-        let bottom_right = Point {
-            x: 10.3,
-            ..another_point
-        };
-
-        // `bottom_right.y` will be the same as `another_point.y` because we used that field
-        // from `another_point`
-        println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
-
-        // Destructure the point using a `let` binding
-        let Point {
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        top_left: Point {
             x: left_edge,
             y: top_edge,
-        } = point;
+        },
+        bottom_right: bottom_right,
+    };
 
-        let _rectangle = Rectangle {
-            // struct instantiation is an expression too
-            top_left: Point {
-                x: left_edge,
-                y: top_edge,
-            },
-            bottom_right: bottom_right,
-        };
+    // Instantiate a unit struct
+    let _unit = Unit;
 
-        // Instantiate a unit struct
-        let _unit = Unit;
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
 
-        // Instantiate a tuple struct
-        let pair = Pair(1, 0.1);
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
 
-        // Access the fields of a tuple struct
-        println!("pair contains {:?} and {:?}", pair.0, pair.1);
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
 
-        // Destructure a tuple struct
-        let Pair(integer, decimal) = pair;
-
-        println!("pair contains {:?} and {:?}", integer, decimal);
-    }
+    println!("pair contains {:?} and {:?}", integer, decimal);
 }
 
 fn advanced_standard_fmt_1() {
@@ -122,4 +111,9 @@ fn advanced_standard_fmt_1() {
 
     // The problem with `derive` is there is no control over how the results look. What if I want this to just show a `7`?
     println!("Now {:?} will print!", Deep(Structure(7)));
+}
+
+fn main() {
+    struct_learning();
+    // advanced_standard_fmt_1();
 }
