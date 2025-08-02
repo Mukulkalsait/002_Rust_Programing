8411114b06d3dd3516f59da3e6af9f04a9eaa607
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-02 21:11:43 +0530

    Learned Formated Print

--- chapter_6_print/src/main.rs
+++ chapter_6_print/src/main.rs
@@ -26,26 +26,44 @@ fn print_2() {
         other_type = 24,
         char_type = '😎'
     );
+    println!("My name is {0}, {1} {0}", "Bond", "James");
 }
 
-// Y: We can treat arguments like its an Array and print them with index 0,1,2,3 & ..n .
+// G: A.use of  std::fmt "{}" properly.
 fn print_3() {
-    println!("  _________________________");
+    // Y: Converting Numbers.
+    println!("_________________________");
     println!("Base 10:               {}", 69420); // 69420
     println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
+    println!("this is binary of number 13: {:b}", 13);
     println!("Base 8 (octal):        {:o}", 69420); // 207454
     println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
-
-    println!("this is binary of number 13: {:b}", 13);
-    format!("string binary is giving Error");
+    println!("string binary is giving Error"); // R:  Will error.
 }
 
-// Y: We can treat arguments like its an Array and print them with index 0,1,2,3 & ..n .
+// G2: B.use of  std::fmt "{}" properly.
 fn print_4() {
-    println!("  _________________________");
+    println!("_________________________");
+    println!("||||||||||");
+    println!("{number: >5}", number = 1); // ____5
+    println!("{number:x<5}", number = 1); // 5xxxx
+    println!("{number:0>5}", number = 1); // 00001
+    println!("{number:0<5}", number = 1); // 10000
+    println!("{number:*>width$}", number = 1, width = 5); // same as above but taking it as width.
+    println!("{number:*>x$}", number = 1, x = 12);
+
+    // B: Rust 1.58+ Direct capture ARG from a surrounding variable.
+    let number: f64 = 1.0;
+    let width: usize = 5; // IMP: width is total width ∴ min is 2 in this case.
+    println!("{number:#<width$}");
 }
 
-// Y: We can treat arguments like its an Array and print them with index 0,1,2,3 & ..n .
+// G: Dead Code and formtating.
 fn print_5() {
-    println!("  _________________________");
+    println!("_________________________");
+    #[allow(dead_code)] // disable `dead_code` which warn against unused module
+    struct Structure(i32);
+
+    // `Structure` does not implement fmt::Display so the billow line will Error.
+    // println!("This struct `{}` won't print...", Structure(3));
 }
