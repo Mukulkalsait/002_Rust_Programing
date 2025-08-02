9ad8da1f7eb51ace7b4efb9e8a57c970c1b0279b
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-02 21:13:19 +0530

    Learned Formated Print

--- chapter_6_print/src/main.rs
+++ chapter_6_print/src/main.rs
@@ -4,6 +4,13 @@ fn main() {
     print_3();
     print_4();
     print_5();
+    /* Y:
+     * format!: write formatted text to String
+     * print!: same as format! but the text is printed to the console (io::stdout).
+     * println!: same as print! but a newline is appended.
+     * eprint!: same as print! but the text is printed to the standard error (io::stderr).
+     * eprintln!: same as eprint! but a newline is appende
+     */
 }
 
 // G: We can treat arguments like its an Array and print them with index 0,1,2,3 & ..n .
