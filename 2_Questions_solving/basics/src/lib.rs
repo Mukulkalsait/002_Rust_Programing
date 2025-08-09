d22456ee2404598abfe1a0b7930b0492eb80ad30
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-09 22:36:52 +0530

    lib and main undersatnding.

---
+++ 2_Questions_solving/basics/src/lib.rs
@@ -0,0 +1,8 @@
+/* Y:
+ * ---------------------------------------
+ * | A CENTERAL-HUB fro all the modules. |
+ * ---------------------------------------
+ *  1. call them here and mdaintain them.
+ *
+ */
+pub mod functions;
---
+++ 2_Questions_solving/basics/src/main.rs
@@ -0,0 +1,37 @@
+use basics::functions::create_user_from_data::users_data;
+use basics::functions::evenodd::{
+    difference_between_same_name_types, even_or_odd, even_or_odd_with_reference,
+};
+use basics::functions::voter_eligiblity::return_eligiblity;
+
+/* Y: 1. Odd Even*/
+fn run_even_odd_test() {
+    let n: i32 = 12;
+    println!(
+        " The Same even odd but by passing reference in function so {n} is {} number ",
+        even_or_odd_with_reference(&n).1
+    );
+    println!(" The number {n} is {} ", even_or_odd(n).1);
+}
+
+/* Y: 2. Eligibility Test*/
+fn run_eligibliligy_tests() {
+    let mut all_people = users_data();
+
+    for i in &mut all_people {
+        i.is_eligibie = return_eligiblity(i.eligibility_age, i.age);
+        println!("{:?}", i);
+    }
+}
+
+/* TAG: EXTRAS*/
+fn _run_other_dependency_functions() {
+    difference_between_same_name_types();
+}
+
+fn main() {
+    run_even_odd_test();
+    run_eligibliligy_tests();
+
+    // run_other_dependency_functions();
+}
