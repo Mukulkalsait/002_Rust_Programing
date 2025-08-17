1a519d58612a4f60b9dada681e5f591539093f9f
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-17 17:38:56 +0530

    main

--- chapter_9_hashmap/src/main.rs
+++ chapter_9_hashmap/src/main.rs
@@ -1,65 +1,66 @@
-use std::collections::HashMap;
-
-fn create_hash_map() -> HashMap<String, i32> {
-    /* Y: 1. TYPE DEFINED */
-    let mut _map_type_defined: HashMap<String, u32> = HashMap::new();
-
-    /* Y: 1. TYPE UN-Defined. */
-    let mut map_type_undefined = HashMap::new();
-    map_type_undefined.insert(String::from("alice"), 15);
-    map_type_undefined.insert(String::from("bob"), 12);
-
-    map_type_undefined
-}
-
-fn access_map_values(map: HashMap<String, i32>) -> HashMap<String, i32> {
-    let key = "alice";
-    if let Some(score) = map.get("alice") {
-        println!(
-            "Finding Val With Key Pare + Error Handling.\n ∴ {} Score is {}",
-            key, score
-        );
-    } else {
-        println!("{} not found.", key)
-    }
-    map
-}
-fn remove_item_from_hash_map(mut map: HashMap<String, i32>) {
-    map.remove("alice");
-}
-
-fn for_loop_in_hash_map(map: HashMap<String, i32>) {
-    for (key, value) in &map {
-        println!("{} : {}", key, value);
-    }
-    remove_item_from_hash_map(map);
-}
-
-fn print_hash_methods(mut map: HashMap<String, i32>) {
-    // G: printing direct.
-    println!("{:?}", map);
-
-    // G: accessing values.
-    map = access_map_values(map);
-    map.insert(String::from("Mukul"), 99);
-
-    // Y: entry() => check key | or_insert() -> if not found add.
-    map.entry(String::from("Mukul")).or_insert(99);
-    map.entry(String::from("Shruti")).or_insert(75);
-
-    for_loop_in_hash_map(map);
-}
+use chapter_9_hashmap::map_functions::create_hash_map::create_hash_map;
+use chapter_9_hashmap::map_functions::operation_in_hash_map::remove_item_from_hash_map;
+use chapter_9_hashmap::map_functions::printing_hash_map::print_hash_methods;
 
 fn main() {
-    let map = create_hash_map();
-    print_hash_methods(map);
+    // G: Tupil HashMap return to mutable.
+    let mut maps = create_hash_map();
+
+    // G: passing as mutable reference.
+    remove_item_from_hash_map(&mut maps[1]);
+
+    // G: passing as immutable reverence.
+    print_hash_methods(&maps[1]);
 
     /* |------------------------------------------------------------------------------------------------------------
-     * DX: Common Confussion in HASHMAP.                                                                            |
+     * IMP : The Mutable reference + immutable reference conclusion.                                                |
      * |------------------------------------------------------------------------------------------------------------
-     *     1. Ownership & Borrowing: Strings & complex keys are moved unless you pass references.
-     *     2. Copy vs Clone: Integers copy by default, Strings must be cloned if you want to reuse after inserting.
-     *     3. Order is not guaranteed — HashMap is unordered.
-     *     4. .get() returns a reference → sometimes you must dereference it with *.
+     *
+     * G: Tupil HashMap return to mutable.
+     *   let mut maps = create_hash_map();
+     *
+     * G: passing as mutable reference.
+     *   remove_item_from_hash_map(&mut maps[1]);
+     *
+     * G: passing as immutable reverence.
+     *   print_hash_methods(&maps[1]);
      */
 }
+
+/*
+ * |------------------------------------------------------------------------------------------------------------
+ * Y: Harshible and harsh types.                                                                               |
+ * |------------------------------------------------------------------------------------------------------------
+ *  “Hashable” — What Does It Really Mean?
+ *  When I say a key type must be hashable, it means:
+ *  The type must be able to produce a hash value (an integer) that represents its contents.
+ *  That hash value is used internally by the HashMap to decide where to store the key-value pair in memory so it can retrieve it quickly.
+ *  In Rust, this means the type must implement the Hash trait.
+ *  Hash in data structures
+ *  This is not the same thing as password hashing (like bcrypt or Argon2), which is designed to be slow and irreversible for security.
+ *  Hashing for a HashMap is fast and deterministic — same input → same hash → quick lookup.
+ *  Example:
+ *  "alice"  → hash function → 932847232
+ *  "bob"    → hash function → 120398473
+ *
+ * |------------------------------------------------------------------------------------------------------------
+ * DX: Common Confussion in HASHMAP.                                                                            |
+ * |------------------------------------------------------------------------------------------------------------
+ *     1. Ownership & Borrowing: Strings & complex keys are moved unless you pass references.
+ *     2. Copy vs Clone: Integers copy by default, Strings must be cloned if you want to reuse after inserting.
+ *     3. Order is not guaranteed — HashMap is unordered.
+ *     4. .get() returns a reference → sometimes you must dereference it with *.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
