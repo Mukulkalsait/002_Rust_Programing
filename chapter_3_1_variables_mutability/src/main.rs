4840377b5bb09a1a21ecf0103d4222338a906bd5
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-21 23:09:22 +0530

    Updatex

--- chapter_3_1_variables_mutability/src/main.rs
+++ chapter_3_1_variables_mutability/src/main.rs
@@ -54,4 +54,5 @@ fn main() {
      */
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("{THREE_HOURS_IN_SECONDS}"); // we can also print like this.
+                                          // print
 }
